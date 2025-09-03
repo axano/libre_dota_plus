213b476c123ab09a043bb53c145fe95b05ae1397
axano <okmanolos1@hotmail.com>
2025-09-03 23:15:49 +0200

    added sound functionality and mp3s

--- lib/timers/Clock.py
+++ lib/timers/Clock.py
@@ -1,15 +1,30 @@
 import time
 import threading
-
+from gtts import gTTS
 import lib.logger
 from settings import *
-
+from playsound3 import playsound
 
 class Clock:
     def __init__(self):
         self.start_time = None
         self._stop_flag = False
         self._thread = None
+        self.last_time_line_creeps_spawned = None
+        self.last_time_rune_spawned = None
+        self.last_time_bounty_rune_spawned = None
+        self.last_time_experience_rune_spawned = None
+        self.last_time_lotus_spawned = None
+        self.last_time_tormentor_spawned = None
+        self.last_time_forrest_creeps_spawned = None
+        self.sound_file_path = "sounds/alert.mp3"
+        self.bounty_rune_spawning_sound_path = "sounds/bounty_runes_spawning.mp3"
+        self.runes_spawning_sound_path = "sounds/runes_spawning.mp3"
+        self.wisdom_rune_spawning_sound_path = "sounds/wisdom_runes_spawning.mp3"
+        self.forrest_creeps_spawning_sound_path = "sounds/forrest_creeps_spawning.mp3"
+        self.lane_creeps_spawning_sound_path = "sounds/lane_creeps_spawning.mp3"
+        self.lotus_spawning_sound_path = "sounds/lotus_spawning.mp3"
+        self.tormentor_spawning_sound_path = "sounds/tormentor_spawning.mp3"
 
     def start(self):
         self.start_time = time.time()
@@ -33,26 +48,58 @@ class Clock:
             h, m, s, total_seconds = self.get_time_in_hours_minutes_seconds_and_total_seconds_passed()
             self.check_lane_creep_spawn(total_seconds)
             self.check_forrest_creep_spawn(total_seconds, m)
-            self.check_rune_spawn(total_seconds)
-            time.sleep(0.5)
+            self.check_rune_spawn(total_seconds, m)
+            self.check_bounty_rune_spawn(total_seconds)
+            self.check_tormentor_spawn(total_seconds, m)
+            self.check_lotus_spawn(total_seconds, m)
+            self.check_wisdom_rune_spawn(total_seconds, m)
+            time.sleep(0.4)
 
     def check_forrest_creep_spawn(self, total_seconds, m):
-        if total_seconds % FORREST_CREEPS_SPAWN_FREQUENCY_IN_SECONDS == 0 and m != 0:
+        if total_seconds % FORREST_CREEPS_SPAWN_FREQUENCY_IN_SECONDS == 0 and m != 0 and self.last_time_forrest_creeps_spawned != total_seconds:
+            self.last_time_forrest_creeps_spawned = total_seconds
             lib.logger.info("Forrest creeps spawned", 4)
-            self.beep()
+            self.play_sound(self.forrest_creeps_spawning_sound_path)
 
-    def check_rune_spawn(self, total_seconds):
-        if total_seconds % RUNE_SPAWN_FREQUENCY_IN_SECONDS == 0:
+    def check_rune_spawn(self, total_seconds, m):
+        if total_seconds % RUNE_SPAWN_FREQUENCY_IN_SECONDS == 0 and m != 0 and self.last_time_rune_spawned != total_seconds:
+            self.last_time_rune_spawned = total_seconds
             lib.logger.info("Runes spawned", 4)
-            self.beep()
+            self.play_sound(self.runes_spawning_sound_path)
+
+    def check_wisdom_rune_spawn(self, total_seconds, m):
+        if total_seconds % WISDOM_RUNE_FREQUENCY_IN_SECONDS == 0 and m != 0 and self.last_time_experience_rune_spawned != total_seconds:
+            self.last_time_experience_rune_spawned = total_seconds
+            lib.logger.info("Wisdom runes spawned", 4)
+            self.play_sound(self.wisdom_rune_spawning_sound_path)
+
+    def check_lotus_spawn(self, total_seconds, m):
+        if total_seconds % LOTUS_SPAWN_FREQUENCY_IN_SECONDS == 0 and m != 0 and self.last_time_lotus_spawned != total_seconds:
+            self.last_time_lotus_spawned = total_seconds
+            lib.logger.info("Lotuses spawned", 4)
+            self.play_sound(self.lotus_spawning_sound_path)
+
+    def check_tormentor_spawn(self, total_seconds, m):
+        if total_seconds % TORMENTOR_SPAWN_FREQUENCY_IN_SECONDS == 0 and m != 0 and self.last_time_tormentor_spawned != total_seconds:
+            self.last_time_tormentor_spawned = total_seconds
+            lib.logger.info("Tormentors spawned", 4)
+            self.play_sound(self.tormentor_spawning_sound_path)
+
+    def check_bounty_rune_spawn(self, total_seconds):
+        if total_seconds % BOUNTY_RUNE_SPAWN_FREQUENCY_IN_SECONDS == 0 and self.last_time_bounty_rune_spawned != total_seconds:
+            self.last_time_bounty_rune_spawned = total_seconds
+            lib.logger.info("Bounty runes spawned", 4)
+            self.play_sound(self.bounty_rune_spawning_sound_path)
 
     def check_lane_creep_spawn(self, total_seconds):
-        if total_seconds % LANE_CREEPS_SPAWN_FREQUENCY_IN_SECONDS == 0:
+        if total_seconds % LANE_CREEPS_SPAWN_FREQUENCY_IN_SECONDS == 0 and self.last_time_line_creeps_spawned != total_seconds:
+            self.last_time_line_creeps_spawned = total_seconds
             lib.logger.info("Lane creeps spawned", 4)
-            self.beep()
-            time.sleep(1)  # prevent double-beep in the same second
+            self.play_sound(self.lane_creeps_spawning_sound_path)
 
-    def beep(self):
+    def play_sound(self, sound):
         h, m, s, total = self.get_time_in_hours_minutes_seconds_and_total_seconds_passed()
-        lib.logger.info(f"Internal timer is {h:02}:{m:02}:{s:02}",4)
-        lib.logger.info("BEEP", 4)
+        # lib.logger.info(f"Internal timer is {h: 02}: {m: 02}: {s: 02}", 4)
+        threading.Thread(target=playsound, args=(sound,), daemon=True).start()
+
+
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import requests
-import lib.logger
-from bs4 import BeautifulSoup
-import lib.data_retrieval.dota2_fandom
 import lib.timers.Clock
 import time
 
-time_to_sleep = 180
+from gtts import gTTS
 
-clock = lib.timers.Clock.Clock()
-clock.start()
-time.sleep(time_to_sleep)
+def main ():
+    clock = lib.timers.Clock.Clock()
+    input("Press enter to start timer")
+    clock.start()
+    while True:
+        time.sleep(1)
+
+
+def debug():
+
+
+    chat_to_speech("Wisdom runes spawning.")
+
+def chat_to_speech(text):
+    # Convert text to speech
+    tts = gTTS(text=text, lang="en" , tld="us")
+    filename = "sounds/response.mp3"
+    tts.save(filename)
+
+
+#debug()
+main()
